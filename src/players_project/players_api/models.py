840c8a2dee302a37e2892039f05519779411cdbf
Freddie <f.groom@dectech.co.uk>
2019-05-31 16:29:50 +0100

    migrated database

--- src/players_project/players_api/models.py
+++ src/players_project/players_api/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Player(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    player_age = models.IntegerField(max_length=3)
+    player_age = models.IntegerField
