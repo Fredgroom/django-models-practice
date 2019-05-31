350819475f4cf6a9be6928d1a12c0466f7753141
Freddie <f.groom@dectech.co.uk>
2019-05-31 11:37:31 +0100

    Player models

--- src/players_project/players_api/models.py
+++ src/players_project/players_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    player_age = models.IntegerField(max_length=3)
